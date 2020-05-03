2f75b27895b34a6f91ec7994cd738f5266e392f5
MartinCalvete <63985377+MartinCalvete@users.noreply.github.com>
2020-05-03 11:28:40 -0300

    Add files via upload

---
+++ VisorDeImagenes/VisorDeImagenes/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    'Martin Calvete 3BF
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            PictureBox1.Load(OpenFileDialog1.FileName)
+
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        PictureBox1.Image.Dispose()
+        PictureBox1.Image = Nothing
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        ColorDialog1.ShowDialog()
+        PictureBox1.BackColor = ColorDialog1.Color
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Close()
+
+    End Sub
+End Class
